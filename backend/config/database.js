b21c088fc8169f078732057601b1f8ced55f5120
akash70629 <akash70629@gmail.com>
2022-09-30 23:49:12 +0530

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+// const MONGO_URI = "mongodb+srv://Akash:abcdefg@akash.cuwbf1d.mongodb.net/test"
+
+exports.connectDatabase = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then((con) => console.log(`Database Connected: ${con.connection.host}`))
+    .catch((err) => console.log(err));
+};
