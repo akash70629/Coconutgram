28e2fecdacf056ae51c068725d8c056dc940fce5
akash70629 <akash70629@gmail.com>
2023-02-18 22:41:51 +0530

    test

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,5 +6,6 @@ exports.connectDatabase = () => {
   mongoose
     .connect(process.env.MONGO_URI)
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
+    .set("strictQuery", false);
 };
